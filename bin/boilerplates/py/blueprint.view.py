c11c5336836fd9b85e43fc6f248f8bf88c2fa00d
Alan Telles <alantelles86@gmail.com>
2021-02-24 01:17:37 -0300

    import render decorator

--- bin/boilerplates/py/blueprint.view.py
+++ bin/boilerplates/py/blueprint.view.py
@@ -1,6 +1,7 @@
 from flask import render_template, redirect, request, url_for, session, flash
 from app.views.base_views import BaseViews
 import app.blueprints.user_management.views.access_control as access_control
+from dont_touch.core_views import render
 
 class {view_name_camel}Views(BaseViews):    
 {views_list}
