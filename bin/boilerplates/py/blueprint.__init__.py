adad7ca71df2c5fc9a44a46f9ebebb0ac794fdef
Alan Telles <alantelles86@gmail.com>
2021-02-24 01:30:09 -0300

    fix: render with decorator blueprint 3

--- bin/boilerplates/py/blueprint.__init__.py
+++ bin/boilerplates/py/blueprint.__init__.py
@@ -17,7 +17,7 @@ from dont_touch.helpers.routes import register_routes
 class {bp_name_camel}Views(BaseViews):
     
     def index(self):
-        self.render('/blog/index')
+        return self.render('/blog/index')
 
 {bp_name}_views = {bp_name_camel}Views()
 
